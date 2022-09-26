85bd59731dc931dd3e6452cf7e7f6847c050e295
Rodrigo Barraza <hello@rod.dev>
2022-09-25 18:46:01 -0700

    Path fix

--- services/EventService/EventService.js
+++ services/EventService/EventService.js
@@ -1,7 +1,7 @@
 const EventService = (router) => {
     const resourceName = 'event-service';
-    const postEvent = require('./routes/postEvent')();
-    const postSession = require('./routes/postSession')();
+    const postEvent = require('./routes/PostEvent')();
+    const postSession = require('./routes/PostSession')();
 
     router.post(`/${resourceName}/event`, postEvent);
     router.post(`/${resourceName}/session`, postSession);
